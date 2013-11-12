d4a91f8cf65827fa16ee716c0f213fe29f65989d
maxdavila <adavila0711@gmail.com>
2013-11-11 16:19:20 -0800

    add rails generated app no test framework V 4.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newvo::Application.config.secret_key_base = '2ac35f3e3ba07c6394e8439cbabeecf11cf75037e061edee484537f82bd35515a9564df17a4d54ffd0f587eb6c733657a637c3f28509e000d3fed5438bd52cd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Newvo::Application.config.session_store :cookie_store, key: '_newvo_session'
