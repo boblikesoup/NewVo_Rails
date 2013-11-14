b093e9d93f1b96c2459b15e9cf380729c023534e
maxdavila <adavila0711@gmail.com>
2013-11-14 02:59:47 -0800

    user can sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Newvo::Application.routes.draw do
   root to: 'pages#home'
 
   get '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/failure', to: redirect('/')
+  get '/signout', to: 'sessions#destroy', as: 'signout'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
