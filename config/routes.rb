6ad85f61349eeac7cba7d1d423eb4735af7c099c
maxdavila <adavila0711@gmail.com>
2013-11-13 17:27:51 -0800

    add oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Newvo::Application.routes.draw do
   root to: 'pages#home'
-  
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
