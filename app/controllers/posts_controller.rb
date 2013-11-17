0780cb18b31e79422784777dfa810fa45517532b
maxdavila <adavila0711@gmail.com>
2013-11-17 02:35:09 -0800

    add paperlcip validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,28 @@
 class PostsController < ApplicationController
+
   def index
     @post = Post.new
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
+
+  def create
+    post = Post.new( post_params )
+    current_user.posts << post 
+    if post.save
+      redirect_to posts_path
+    else 
+      # TODO 
+      # display errors and prevent cookie overflow when content type is not an image
+      post.picture1.destroy
+      flash[:error] = post.errors
+      redirect_to posts_path
+    end
+  end
+
+  private 
+
+  def post_params
+    params.require( :post ).permit( :title, :picture1 )
+  end
+
 end
