590f4bedd752251e1dcd1b28f91d17feec80f3d5
maxdavila <adavila0711@gmail.com>
2013-11-17 00:02:03 -0800

    user can create post with one picture

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,16 @@ class PostsController < ApplicationController
     @post = Post.new
     @posts = Post.all
   end
+
+  def create
+    post = Post.create( post_params )
+    redirect_to posts_path
+  end
+
+  private 
+
+  def post_params
+    params.require( :post ).permit( :title, :picture1 )
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-    has_attached_file :picture_1, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    has_attached_file :picture1, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
