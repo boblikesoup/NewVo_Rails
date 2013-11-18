d103bbbced1c0553be586c8acfd85a89d1161c45
maxdavila <adavila0711@gmail.com>
2013-11-17 16:50:31 -0800

    user can edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,16 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes! post_params
+    redirect_to posts_path
+  end
+
   private 
 
   def post_params
