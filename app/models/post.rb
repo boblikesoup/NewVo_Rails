041d99c6cc82f8461cf4f70f4bf4998b4787ee47
Juke <apprentice@Jukes-lappy.local>
2013-11-20 17:27:38 -0800

    added user factory/Factory Girl

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
     belongs_to :user
     has_many :comments
 
+
     has_attached_file :picture1, :styles => { :medium => "300x300>", :thumb => "100x100>" }
     validates_attachment :picture1, :presence => true,
       :content_type => { :content_type => %w(image/jpg image/png image/jpeg)},
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :fb_uid
+  validates_uniqueness_of :fb_uid
+
+
   def self.find_or_create_from_auth_hash auth_hash
     user = self.find_or_create_by(fb_uid: auth_hash["uid"])
     if user
