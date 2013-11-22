75e9e7a5f296b678fa9173e48460cb2c5e82544e
maxdavila <adavila0711@gmail.com>
2013-11-22 01:00:24 -0800

    add vote after_save callback

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,9 +1,15 @@
 class VotesController < ApplicationController
   def create
+    # need to delete previous user vote on the backend so they are able
+    # to upvote, 
+    vote = Vote.new(user_id: current_user.id, value: params[:value])
     if params[:photo]
-      vote = Vote.new(user_id: current_user.id, value: params[:value])
       current_photo = Photo.find(params[:photo])
       current_photo.votes << vote
+      vote.save
+    else
+      current_comment = Comment.find(params[:comment])
+      current_comment.votes << vote
       vote.save
     end
       redirect_to posts_path
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,13 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :votable, polymorphic: true #, counter_cache: true
   validates_uniqueness_of :user_id, scope: [:votable_id, :votable_type, :value]
+  after_save :destroy_previous_vote
+  
+  private
 
+  def destroy_previous_vote
+    return if !self.votable.post.has_single_picture?
+    previous_vote = self.votable.votes.where(user_id: self.user_id).take
+    previous_vote.destroy if previous_vote != self
+  end
 end
