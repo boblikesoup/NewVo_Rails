5b22022965dca85c581aa962fa7ee7225fab1b85
John Uke <johnuke@gmail.com>
2013-12-19 16:49:03 -0800

    created 'followings' table where users can follow each other.  missing ability to easily select followers easily

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   has_many :votes, :dependent => :destroy
   has_many :photos, through: :posts
+
+  has_many :followings, foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followed_users, through: :followings, source: :followed
+  has_many :following_users, through: :followings, source: :follower
+  #user.followed_users to find all current user is following
+  #user.following_users to find all following current user
+  # above functionality of selection is not currently working
+
+
   # validates_presence_of :first_name
   # validates_presence_of :last_name
   # validates_presence_of :username
@@ -11,6 +22,18 @@ class User < ActiveRecord::Base
   # validates_uniqueness_of :email
   # validates_presence_of :fb_uid
   # validates_uniqueness_of :fb_uid
+
+  def following?(other_user)
+    followings.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    followings.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    followings.find_by_followed_id(other_user.id).destroy
+  end
 
 #Store unique username and email address
   def self.find_or_create_from_auth_hash auth_hash
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131217232431) do
+ActiveRecord::Schema.define(version: 20131219235124) do
 
   create_table "comments", force: true do |t|
     t.string   "body"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20131217232431) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "followings", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "followings", ["followed_id"], name: "index_followings_on_followed_id"
+  add_index "followings", ["follower_id", "followed_id"], name: "index_followings_on_follower_id_and_followed_id", unique: true
+  add_index "followings", ["follower_id"], name: "index_followings_on_follower_id"
 
   create_table "photos", force: true do |t|
     t.integer  "post_id"
