e8efb2098769cc3931e50387473bf187ee41fdbf
Brent <bjgaynor@gmail.com>
2014-01-11 20:02:18 -0800

    Post Validations Completed
    add .env file
    add validations for title, user_id, photos
    change default has_single_picture to false
    change method as well

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments#, :dependent => :destroy
-  has_many :photos#, :dependent => :destroy
+  has_many :comments
+  has_many :photos
   accepts_nested_attributes_for :photos,
       :reject_if => lambda { |attributes| attributes[:photo].blank? }
 
   after_save :update_has_single_picture
 
-  # validates_presence_of :title
-  # validates_presence_of :user_id
-  # validates_presence_of :single
+  validates_presence_of :title
+  validates_presence_of :user_id
+  validates_presence_of :photos
+
 
   def as_json(options={})
     {
@@ -24,6 +25,6 @@ class Post < ActiveRecord::Base
   private
 
   def update_has_single_picture
-    self.update_columns(has_single_picture: 'false') if self.photos.count == 2
+    self.update_columns(has_single_picture: 'true') if self.photos.count == 1
   end
 end
