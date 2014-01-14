30678bfb6a8908af084d5fe9bdb557d2b31b3302
John Uke <johnuke@gmail.com>
2014-01-14 15:11:25 -0800

    added User papaerclip interpolation

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,7 @@
 Paperclip::interpolates :post_id do |attachment, style|
   attachment.instance.post_id
 end
+
+Paperclip::interpolates :user_id do |attachment, style|
+  attachment.instance.user_id
+end
