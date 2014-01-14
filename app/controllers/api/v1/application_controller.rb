9d0908ae45c3df5779f407730a7d11d1c5128f5a
John Uke <johnuke@gmail.com>
2014-01-14 13:09:15 -0800

    uncommented an end that was breaking

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -16,4 +16,5 @@ class API::V1::ApplicationController < ActionController::Base
   def set_current_user
       User.current = current_user
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -100,7 +100,7 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_user_info user_info
     user = self.find_or_create_by(fb_uid: user_info["id"])
-    # if user
+    if user
     #   first_name = user_info["info"]["first_name"]
     #   last_name = user_info["info"]["last_name"]
     #   avatar = user_info["info"]["image"]
