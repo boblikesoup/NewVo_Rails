14ede976530e490529f853f4dc41e67bd9e8ec4c
John Uke <johnuke@gmail.com>
2014-01-14 15:34:10 -0800

    rmoving attached file

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,6 @@ Paperclip::interpolates :post_id do |attachment, style|
   attachment.instance.post_id
 end
 
-Paperclip::interpolates :uid do |attachment, style|
-  attachment.instance.uid
-end
+# Paperclip::interpolates :uid do |attachment, style|
+#   attachment.instance.uid
+# end
