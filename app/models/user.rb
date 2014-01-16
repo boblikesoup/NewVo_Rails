1200ba23cd5672db6c581dd37dfa3dcef82e6fe7
John Uke <johnuke@gmail.com>
2014-01-16 12:46:40 -0800

    some change to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -119,6 +119,7 @@ class User < ActiveRecord::Base
       :following_users => self.following_users,
       :friends => self.friends,
       :posts => posts.order("created_at desc").limit(6),
+      :fb_token =>
     }
   end
 
