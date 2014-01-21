818d2334836997ec83145716751927846cbb7f10
John Uke <johnuke@gmail.com>
2014-01-20 17:47:23 -0800

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -71,8 +71,20 @@ ActiveRecord::Schema.define(version: 20140120014901) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "profile_pic"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.string   "newvo_token"
   end
+
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "votes", force: true do |t|
     t.integer  "votable_id"
