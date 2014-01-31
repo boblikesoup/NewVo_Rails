4f291864e3d92cbe7a10c8c342b62cb61d4580d7
Brent <bjgaynor@gmail.com>
2014-01-30 18:49:03 -0800

    ssl for pictures added must test on heroku, change http assets to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ Newvo::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS+ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_protocol => "https",
     },
     :path => '/:attachment/:post_id/:style/:filename'
   }
