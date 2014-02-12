95e552910b958381db891c87f3ac708e1e05e2c2
Brent <bjgaynor@gmail.com>
2014-02-11 17:02:06 -0800

    removed broken and unused destroy_previous_vote method from Vote.rb

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -13,9 +13,6 @@ class Vote < ActiveRecord::Base
   validates_presence_of :post_id
   validates_uniqueness_of :user_id, scope: [:votable_id, :votable_type, :value]
 
-  # broken!
-  # after_save :destroy_previous_vote
-
   def as_json(options={})
     {
       :id => id,
@@ -27,20 +24,5 @@ class Vote < ActiveRecord::Base
       :photo => Photo.find(self.votable_id).photo.url(:thumb)
     }
   end
-
-  private
-
-  # Method is broken due to the post in this line "if self.votable.post.has_single_picture?"
-  # Try a = Vote.create and then a.votable
-  # Tests break because votable returns nil and there is no method #post for nil
-
-  # def destroy_previous_vote
-  #   if self.votable.post.has_single_picture? or self.votable.is_a?(Comment)
-  #     previous_vote = self.votable.votes.where(user_id: self.user_id).take
-  #   else
-  #     previous_vote = Vote.where(user_id: self.user_id, post_id: self.post_id).take
-  #   end
-  #     previous_vote.destroy if previous_vote != self
-  # end
 
 end
