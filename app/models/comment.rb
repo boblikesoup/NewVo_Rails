07759633bf27a18972995cbb59a316706fe375b7
John Uke <johnuke@gmail.com>
2014-02-13 10:36:28 -0800

    changed comment fb_username is first name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -21,7 +21,7 @@ class Comment < ActiveRecord::Base
       :user_id => user_id,
       :profile_pic => User.find(user_id).profile_pic,
       :post_id => post_id,
-      :user_name => User.find(user_id).facebook_username
+      :user_name => User.find(user_id).first_name
     }
   end
 
