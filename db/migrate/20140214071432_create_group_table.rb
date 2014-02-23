5f33dd3f28700de4bbeee4f523d1a3f90e6a10cb
Brent <bjgaynor@gmail.com>
2014-02-23 05:51:57 -0800

    groups table fixed

--- db/migrate/20140214071432_create_group_table.rb
+++ db/migrate/20140214071432_create_group_table.rb
@@ -2,7 +2,7 @@ class CreateGroupTable < ActiveRecord::Migration
   def change
     create_table :groups do |t|
       t.integer :creator_id
-      t.integer :user_id
+      t.text :user_id
       t.string :title
       t.string :description
 
