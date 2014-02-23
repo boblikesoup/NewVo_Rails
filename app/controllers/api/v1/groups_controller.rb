d75b7db460a5f65c181e4451dbfab82cb0efcef5
Brent <bjgaynor@gmail.com>
2014-02-23 05:02:55 -0800

    GroupsController define

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,24 +1,25 @@
-def create
-  @group = Group.new(creator_id: @current_user.id, user_id: params[:user_id], title: params[:title], description: params[:description])
-  if @group.save
+class API::V1::FollowingsController < API::V1::ApplicationController
+  def create
+    @group = Group.new(creator_id: @current_user.id, user_id: params[:user_id], title: params[:title], description: params[:description])
+    if @group.save
+      response = {}
+      response["success"] = true
+      response["data"] = @group
+      render json: response
+    else
+      render json: {success: false, message: "There has been a problem creating this group"}
+    end
+  end
+
+  # Not sure exactly how to take serialized params, WIP
+  # def circuit_params
+  #   params.require(:circuit).permit(:title, :id, viewable_tasks:[], ... )
+  # end
+
+  def show
+    @group = Group.find(params[:id])
     response = {}
     response["success"] = true
     response["data"] = @group
-    render json: response
-  else
-    render json: {success: false, message: "There has been a problem creating this group"}
+    respond_with(response)
   end
-end
-
-# Not sure exactly how to take serialized params, WIP
-# def circuit_params
-#   params.require(:circuit).permit(:title, :id, viewable_tasks:[], ... )
-# end
-
-def show
-  @group = Group.find(params[:id])
-  response = {}
-  response["success"] = true
-  response["data"] = @group
-  respond_with(response)
-end
