64fa800b6abb548dfe5ecfb0ea6a3347d987783c
Brent <bjgaynor@gmail.com>
2014-02-25 23:33:28 -0800

    exception handling pro edition

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,16 +14,16 @@ class API::V1::SessionsController < API::V1::ApplicationController
       ENV['FACEBOOK_APP_SECRET'],
       site: 'https://graph.facebook.com')
     facebook_token = OAuth2::AccessToken.new(client, params[:fbtoken])
-    begin
+    # begin
     user_info = ActiveSupport::JSON.decode(facebook_token.get('/me').body)
     picture_info = ActiveSupport::JSON.decode(facebook_token.get('/me?fields=picture').body)
-    rescue OAuth2::Error
-      invalid_login_attempt
-    rescue StandardError => e
-      invalid_login_attempt
-    rescue NoMethodError
-      invalid_login_attempt
-    end
+    # rescue OAuth2::Error
+    #   invalid_login_attempt
+    # rescue StandardError => e
+    #   invalid_login_attempt
+    # rescue NoMethodError
+    #   invalid_login_attempt
+    # end
     if user_info && picture_info
       @user = User.find_or_create_from_user_info(user_info, picture_info)
       if @user
