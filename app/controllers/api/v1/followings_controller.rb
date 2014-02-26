f613be2b7c1fabc46f288c1e18ce0c797d945655
Brent <bjgaynor@gmail.com>
2014-02-26 01:59:08 -0800

    followings delete fixed

--- app/controllers/api/v1/followings_controller.rb
+++ app/controllers/api/v1/followings_controller.rb
@@ -2,13 +2,11 @@ class API::V1::FollowingsController < API::V1::ApplicationController
   respond_to :json
 
   def create
-  begin
     if @current_user.follow!(params[:followed_id])
       render json: {success: true, message: "following created", follower_id: @current_user.id, followed_id: params[:followed_id]}
-      FollowingActivity.create!()
+      # FollowingActivity.create!()
        # CommentActivity.create!(notified_user_id: Post.find(params[:post_id]).user_id, other_user_id: @comment.user_id, comment_id: @comment.id)
     end
-  end
     if @current_user.both_following?(params[:followed_id])
       @current_user.create_friendship(params[:followed_id])
     end
@@ -16,13 +14,15 @@ class API::V1::FollowingsController < API::V1::ApplicationController
 
   def destroy
     #id = other user's id NOT of relationship
-    following = Following.find_by(follower_id: @current_user.id, followed_id: params[:id])
-    if @current_user.both_following?(following.followed_id)
-      @current_user.destroy_friendship(following.followed_id)
-    else
-      FollowingActivity.published.find_by(notified_user_id: @current_user.id, following_id: params[:id], followed_type: "follower").update_attributes(status: FollowingActivity::STATUS_UNPUBLISHED)
-      FollowingActivity.published.find_by(notified_user_id: params[:id], following_id: @current_user.id, followed_type: "followed").update_attributes(status: FollowingActivity::STATUS_UNPUBLISHED)
-    end
+    following = Following.find_by(followed_id: params[:id], follower_id: @current_user.id)
+    # if @current_user.both_following?(following.followed_id)
+    #   @current_user.destroy_friendship(following.followed_id)
+    # else
+    #   FollowingActivity.published.find_by(notified_user_id: @current_user.id, following_id: params[:id], followed_type: "follower").update_attributes(status: FollowingActivity::STATUS_UNPUBLISHED)
+    #   FollowingActivity.published.find_by(notified_user_id: params[:id], following_id: @current_user.id, followed_type: "followed").update_attributes(status: FollowingActivity::STATUS_UNPUBLISHED)
+    # end
+    render json: {success: true, message: "following destroyed", follower_id: @current_user.id, followed_id: following.followed_id}
     following.destroy!
   end
+
 end
