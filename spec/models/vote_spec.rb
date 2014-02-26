10937421c5c3eeb2b8ffa97ff6fe39d48ccc376f
Brent <bjgaynor@gmail.com>
2014-02-25 23:37:40 -0800

    2.0 bitch

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Vote do
   it { should belong_to(:user) }
   it { should belong_to(:post) }
-  it { should belong_to(:votable).counter_cache(true) }
+  it { should belong_to(:votable) }
   it { should validate_presence_of(:user_id) }
   it { should validate_uniqueness_of(:user_id) }
   it { should validate_presence_of(:post_id) }
