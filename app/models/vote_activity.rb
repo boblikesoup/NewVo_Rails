4181fe3e6b8574830f7e134ae34a5bb94237c4a2
Brent <bjgaynor@gmail.com>
2014-03-17 00:50:38 -0700

    fixed assemble_vote bug

--- app/models/vote_activity.rb
+++ app/models/vote_activity.rb
@@ -10,7 +10,7 @@ class VoteActivity < ActiveRecord::Base
     vote_activity[:id] = self.id
     vote_activity[:created_at] = self.created_at
     vote_activity[:other_user] = User.find(self.other_user_id).assemble_user
-    vote_activity[:vote_info] = Vote.find(self.vote_id).assemble_vote
+    # vote_activity[:vote_info] = Vote.find(self.vote_id).assemble_vote
     return vote_activity
   end
 
