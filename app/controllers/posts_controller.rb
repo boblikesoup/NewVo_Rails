20f945104b412ef75e6cba9be7391addc19dd0c0
Brent <bjgaynor@gmail.com>
2014-04-28 21:51:58 -0700

    add api routes to routes.rb outside of namespacing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,7 @@ class PostsController < ApplicationController
     @posts = Post.order(created_at: :desc)
     @posts_page = true
     @profile = false
-
     2.times { @post.photos.build }
-    #render json: @posts, :include => [:photos, :comments]
     respond_with(@posts)
   end
 
@@ -17,9 +15,6 @@ class PostsController < ApplicationController
     current_user.posts << @post
     @post.save
     respond_with @post, :location => posts_path
-      # TODO
-      # display errors and prevent cookie overflow when content type is not an image
-      # handle error when save is not successful
   end
 
   def show
@@ -30,13 +25,11 @@ class PostsController < ApplicationController
 
   def destroy
     post = Post.find(params[:id])
-    #post.destroy!
     post.update_attributes(status: Post::STATUS_UNPUBLISHED)
     respond_with post, :location => posts_path
   end
 
   private
-  #refactor this shit!
   def post_params
     params.require(:post).permit(:description, photos_attributes: [:id, :photo])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Newvo::Application.routes.draw do
   root to: 'pages#home'
 
+  #sessions
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/mobile', to: 'api/v1/sessions#create'
   get '/signout', to: 'sessions#destroy', as: 'signout'
   get '/signout/mobile', to: 'api/v1/sessions#destroy'
+
+  #web
+  patch '/groups/add', to: 'groups#add_members'
+  get '/posts/search', to: 'posts#search'
+  get '/posts/voted_on', to: 'posts#voted_on', as: 'posts_voted_on'
+  get '/posts/not_voted_on', to: 'posts#not_voted_on'
+  get '/posts/commented_on', to: 'posts#commented_on'
+  patch '/users/describe', to: 'users#description'
+  get '/pages/about', to: 'pages#about'
+  get '/pages/legal', to: 'pages#legal'
+  get '/pages/contact_us', to: 'pages#contact_us'
 
   resources :posts, only: [:index, :create, :show, :destroy] do
     resources :comments, only: [:create, :update, :edit, :destroy]
@@ -13,12 +25,17 @@ Newvo::Application.routes.draw do
   resources :votes, only: :create
   resources :followings, only: [:create, :destroy, :show]
   resources :users, only: [:index, :show]
+  resources :groups, only: [:create, :show, :destroy]
 
   namespace :api, :defaults => {:format => :json} do
    namespace :v1 do
     resources :users, only: [:index, :show]
     resources :activity_feed, only: :index
     resources :groups, only: [:create, :show, :destroy]
+    resources :posts, only: [:index, :create, :show, :destroy] do
+    resources :comments, only: [:create, :update, :destroy]
+    end
+    #api
     patch '/groups/add', to: 'groups#add_members'
     get '/posts/search', to: 'posts#search'
     get '/posts/voted_on', to: 'posts#voted_on', as: 'posts_voted_on'
@@ -28,9 +45,6 @@ Newvo::Application.routes.draw do
     get '/pages/about', to: 'pages#about'
     get '/pages/legal', to: 'pages#legal'
     get '/pages/contact_us', to: 'pages#contact_us'
-    resources :posts, only: [:index, :create, :show, :destroy] do
-      resources :comments, only: [:create, :update, :destroy]
-    end
     resources :votes, only: :create
     resources :followings, only: [:create, :destroy]
     end
